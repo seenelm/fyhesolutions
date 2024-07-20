b3b867b256edeada705a2a62a3148be14d78dfaf
Yassine Elmellouki <yelmellouki@gmail.com>
2024-07-19 21:51:41 -0400

    first commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import './style.css'
+import './assets/css/bootstrap.css';
+import './assets/css/style.css';
+import './assets/css/responsive.css';
+import './assets/js/custom.js';
+
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div></div>
+`
+
+
